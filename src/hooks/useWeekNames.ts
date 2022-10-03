8c0709394706fa7092bafcd597812bee22783787
Ruan Moreira de Jesus <ruanmoreiraofc@hotmail.com>
2022-10-02 21:03:57 -0300

    feat: create `useWeekNames` hook

---
+++ src/hooks/useWeekNames.ts
@@ -0,0 +1,25 @@
+import { PureDate } from '@lib/PureDate';
+
+export default useWeekNames;
+
+function useWeekNames(firstDayInCalendarRelativeToSunday: number) {
+  const INITIAL_SUNDAY_DATE_FROM_ZERO_DATE = -3;
+
+  const initialDay =
+    INITIAL_SUNDAY_DATE_FROM_ZERO_DATE - firstDayInCalendarRelativeToSunday;
+
+  const firstDayOfWeek = new PureDate(0).setHours(24 * initialDay, 0, 0, 0);
+
+  const weekNames = Array.from(
+    { length: 7 },
+
+    (_, i) => firstDayOfWeek.setHours(24 * i, 0, 0, 0).asDate,
+  ).map(
+    Intl.DateTimeFormat(undefined, {
+      weekday: 'long',
+      timeZone: 'UTC',
+    }).format,
+  );
+
+  return weekNames;
+}
